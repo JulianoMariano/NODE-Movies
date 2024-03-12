3d17264fcd2777e0832308ccc8744b44e22621b7
Juliano <julianoaugusto.xp6@gmail.com>
2024-03-12 19:33:58 -0300

    estruturação do projeto

--- src/server.js
+++ src/server.js
@@ -1,10 +1,10 @@
 const express = require("express")
 
+const { usersRoutes } = require("./routes/users.routes")
+
 const app = express()
 
-app.get("/message/:id", (request, response) => {
-    response.send(`ID da mensagem: ${request.params.id}`);
-})
+app.use(express.json())
 
 const PORT = 1234
 app.listen(PORT, () => console.log(`Server is running on ${PORT}`))
