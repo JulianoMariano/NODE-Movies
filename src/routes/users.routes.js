2c66f0e0df24019ab94414f42093a5440a338f33
Juliano <julianoaugusto.xp6@gmail.com>
2024-03-15 19:32:44 -0300

    exception handling

--- src/routes/users.routes.js
+++ src/routes/users.routes.js
@@ -4,18 +4,8 @@ const UsersController = require("../controllers/UsersController")
 
 const usersRoutes = Router()
 
-function myMiddleware(request, response, next) {
-    console.log("Você passou pelo MIDDLEWARE")
-
-    if(!request.body.isAdmin){
-       return response.json({ message: "Usuário não tem permissão. Contate o administrador" }) 
-    }
-
-    next()
-}
-
 const usersController = new UsersController()
 
-usersRoutes.post("/",myMiddleware, usersController.create);
+usersRoutes.post("/", usersController.create);
 
 module.exports = usersRoutes;
