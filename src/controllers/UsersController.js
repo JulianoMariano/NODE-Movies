7417446d908eddda345543c0f5369764bed1774a
Juliano <julianoaugusto.xp6@gmail.com>
2024-03-17 12:17:09 -0300

    create checkUserExist

--- src/controllers/UsersController.js
+++ src/controllers/UsersController.js
@@ -1,14 +1,17 @@
 const AppError = require("../utils/AppError")
 
+const sqliteConnection = require("../database/sqlite")
+
 class UsersController {
-    create(request, response ) {
+    async create(request, response ) {
         const { name, email, password } = request.body;
 
-        if(!name){
-            throw new AppError("O nome é obrigatório")
-        }
-
-        response.status(201).json({ name, email, password })
+        const database = await sqliteConnection();
+        const checkUserExist = await database
+            .get("SELECT * FROM users WHERE email = (?)", [email])
+        if(checkUserExist) {
+            throw new AppError("Este e-mail já está em uso, por favor, insira um email válido")
+        }return response.status(201).json()
     }
 }
 
